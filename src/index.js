b8d720453775cc39853dee93ddf96f8a82e6c006
irina <irina.konovalova455@gmail.com>
2021-02-22 01:48:08 +0200

    layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './styles/main.scss';
+
+
 console.log('Hello console!');
--- webpack.common.js
+++ webpack.common.js
@@ -51,7 +51,18 @@ module.exports = {
         }
       },
       {
-        test: /\.(png|svg|jpg|gif)$/,
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif|webp)$/,
         use: [
           'file-loader'
         ]
